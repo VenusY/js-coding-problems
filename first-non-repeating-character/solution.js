8a27cb126101f623ac10f9488c852072fc5223e1
Venus <venusyip@outlook.com>
2025-04-24 02:22:37 +0100

    Add solution for first non-repeating character problem

---
+++ first-non-repeating-character/solution.js
@@ -0,0 +1,25 @@
+function solution(string) {
+  if (typeof string !== 'string') {
+    return 'Invalid input type. Must be a string.';
+  }
+
+  const frequencyMap = new Map();
+
+  for (let i = 0; i < string.length; i++) {
+    if (!frequencyMap.has(string[i])) {
+      frequencyMap.set(string[i], 1);
+    } else {
+      frequencyMap.set(string[i], frequencyMap.get(string[i]) + 1);
+    }
+  }
+
+  for (let [char, frequency] of frequencyMap) {
+    if (frequency === 1) {
+      return char;
+    }
+  }
+
+  return 'None';
+}
+
+console.log(solution(''));
